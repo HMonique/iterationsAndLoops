f991ed93f6c482c284837bf44cbee7efa0bafbda
HMonique <161358207+HMonique@users.noreply.github.com>
2024-05-20 22:50:58 -0400

    iterationsAndLoops

---
+++ app.js
@@ -0,0 +1,28 @@
+// Declared a Variable
+let myLine =`GJJA`;
+
+/** 
+ * Initialize, Condition, Increment
+ * 
+*/
+for(let i = 0; i <= 3; i++) {
+ const newCharacter = myLine[i];
+ console.log(newCharacter);
+}
+
+
+
+/**
+ * Create a for loop and I want you to out put "Hi" 7 times
+ * 
+ * 
+ */
+let count = 7;
+for (let i = 0; i < count; i++) {
+   console.log("Hi");
+}
+
+
+
+
+
